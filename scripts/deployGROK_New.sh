4bdfb7171f1adf3cc66fd07b02dc8a666ae7c60d
Ubuntu <sauravjoshi2000@yahoo.com>
2019-10-11 06:54:33 +0000

    tiny miny changes

---
+++ scripts/deployGROK_New.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Stop the container and remove, then start the new image
+mkdir -p /iaasimov/GROK
+cd /iaasimov/GROK
+/usr/local/bin/docker-compose down
+cd ..
+rm -rf /iaasimov/GROK
+mkdir -p GROK
+unzip /home/opc/iaasimov-dockercompose.zip -d /iaasimov/GROK
+
+cd /iaasimov/GROK
+/usr/local/bin/docker-compose build
+/usr/local/bin/docker-compose up -d
+
